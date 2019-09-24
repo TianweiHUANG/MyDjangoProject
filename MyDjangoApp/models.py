b420bf92ad8feeca2e49364cee9aef4c2665e942
htw01118891 <huangtianwei1988@163.com>
2019-09-24 13:15:31 +0800

    MyDjangoProject

--- MyDjangoApp/models.py
+++ MyDjangoApp/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Tabs(models.Model):
+class Tab(models.Model):
     value_a = models.CharField(max_length=10)
     value_b = models.CharField(max_length=10)
     result = models.CharField(max_length=10)
+
--- MyDjangoApp/views.py
+++ MyDjangoApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from MyDjangoApp.models import Tab
 # Create your views here.
 
 def index(request):
@@ -10,5 +10,6 @@ def result(request):
     value_a = request.POST["valueA"]
     value_b = request.POST["valueB"]
     result= int(value_a)+int(value_b)
-    #print(value_a,value_b,result)
+    print(value_a,value_b,result)
+    Tab.objects.create(value_a=value_a,value_b=value_b,result=result)
     return render(request, "result.html", context={"result_resultHTML":result})
